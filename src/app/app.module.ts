96e9cc360d5419424aad2fee986e8acc1767bdd1
Tomas <fly.laso@gmail.com>
2023-08-24 14:16:19 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
+import { ImageSwapperComponent } from './image-swapper/image-swapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSwapperComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap: [
+  //   AppComponent
+  // ],
+  entryComponents: [
+    ImageSwapperComponent
+  ],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private injector: Injector
+  ) { }
+
+  ngDoBootstrap() {
+    customElements.define('image-swapper', createCustomElement(ImageSwapperComponent, {
+      injector: this.injector
+    }));
+  }
+}
---
+++ src/app/image-swapper/image-swapper.component.ts
@@ -0,0 +1,53 @@
+import { AfterViewInit, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-image-swapper',
+  templateUrl: './image-swapper.component.html',
+  styleUrls: ['./image-swapper.component.scss']
+})
+export class ImageSwapperComponent implements AfterViewInit {
+
+  @Input() prefix: string = '';
+  @Input() ext: string = '';
+  @Input() count: number = 0;
+  @Input() preloadCount: number = 5;
+  @Input() step: number = 1;
+
+  @Input()
+  get current(): number {
+    return this._current;
+  }
+  set current(value: number) {
+    this._current = value;
+    this.generateCurrentImages();
+  }
+
+  get currentImageSrc(): string {
+    return this.generateImageName(this.current);
+  }
+
+  currentImages: string[] = [];
+
+  private _current: number = 0;
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.generateCurrentImages();
+  }
+
+  private generateCurrentImages(): void {
+    const start = Math.max(0, +this.current - this.preloadCount);
+    const count = Math.min(this.count, +this.current + +this.preloadCount + 1) - start;
+    console.log('count', count);
+    this.currentImages = this.generateArray(start, count, this.step).map(index => this.generateImageName(index));
+  }
+
+  private generateImageName(index: number): string {
+    return `${this.prefix}${index}.${this.ext}`;
+  }
+
+  private generateArray(start: number, count: number, step: number): number[] {
+    return Array.from({ length: count }, (_, i) => start + i * step);
+  }
+}
